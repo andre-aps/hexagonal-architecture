2b11adf9207f450b22ac7958255e31628b6ae725
André <andre.psilva@picpay.com>
2024-08-19 22:03:46 -0300

    adding client for address search by zip code

---
+++ src/main/java/com/example/hexagonal/adapters/out/client/FindAddressByZipCodeClient.java
@@ -0,0 +1,14 @@
+package com.example.hexagonal.adapters.out.client;
+
+import com.example.hexagonal.adapters.out.client.response.AddressResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "findAddressByZipCodeClient", url = "${hexagonal.client.address.url}")
+public interface FindAddressByZipCodeClient {
+
+    @GetMapping("/{zipCode}")
+    AddressResponse find(@PathVariable("zipCode") final String zipCode);
+
+}
