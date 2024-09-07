989a664d17470de005fe16449a7d2dc03112d6d6
André <andre.psilva@picpay.com>
2024-09-07 19:52:12 -0300

    adjusting properties

--- src/main/java/com/example/hexagonal/config/KafkaConsumerConfig.java
+++ src/main/java/com/example/hexagonal/config/KafkaConsumerConfig.java
@@ -3,6 +3,8 @@ package com.example.hexagonal.config;
 import com.example.hexagonal.adapters.in.consumer.message.CustomerMessage;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -11,8 +13,14 @@ import org.springframework.kafka.support.serializer.JsonDeserializer;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.AUTO_OFFSET_RESET_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG;
 
+@EnableKafka
+@Configuration
 public class KafkaConsumerConfig {
 
     @Bean
@@ -24,14 +32,14 @@ public class KafkaConsumerConfig {
         props.put(VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put(AUTO_OFFSET_RESET_CONFIG, "earliest");
         return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new
-                JsonDeserializer<>(CustomerMessage.class));
+            JsonDeserializer<>(CustomerMessage.class));
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, CustomerMessage>
     kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, CustomerMessage> factory = new
-                ConcurrentKafkaListenerContainerFactory<>();
+            ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
     }
--- src/main/java/com/example/hexagonal/config/KafkaProducerConfig.java
+++ src/main/java/com/example/hexagonal/config/KafkaProducerConfig.java
@@ -2,6 +2,7 @@ package com.example.hexagonal.config;
 
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -10,8 +11,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG;
-import static org.apache.kafka.clients.producer.ProducerConfig.*;
+import static org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
 
+@Configuration
 public class KafkaProducerConfig {
 
     @Bean
